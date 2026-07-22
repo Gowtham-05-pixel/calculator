464f32a77fc54397e5edc64324dfc51812d0a5f1
Your Name <you@example.com>
2026-07-22 16:33:36 +0530

    Add Topper Function

--- grades.py
+++ grades.py
@@ -10,8 +10,12 @@ def calculate_grade(marks):
     return "C"
  else:
     return "F"
+
+def topper(marks_dict):
+ return max(marks_dict, key=marks_dict.get)
  
 if __name__ == "__main__":
  marks = 82
  print("Marks:", marks)
  print("Grade:", calculate_grade(marks))
+ print("Topper:", topper(marks))
